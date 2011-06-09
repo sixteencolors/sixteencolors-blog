04d54e181713a45085424ee341c86d5a9c9a1782
Brian Cassidy <bricas@cpan.org>
2011-06-09 11:16:20 -0300

    some work to header+footer

--- wp-content/themes/sixteencolors/footer.php
+++ wp-content/themes/sixteencolors/footer.php
@@ -1,2 +1,18 @@
+    <footer class=container>
+        <div class=grid_3>
+            <p>(c) 2011 Sixteen Colors</p>
+            <p class=designedby>Artwork by enzO [<a href="http://blocktronics.net/">b7</a>]</p>
+        </div>
+        <div class=grid_2>
+        </div>
+        <div class=grid_4 id=connect>
+            <h2>Connect with 16c...</h2>
+            <ul>
+                <li><a href="http://twitter.com/sixteencolors">Twitter</a> <a href="http://facebook.com/sixteencolors">Facebook</a></li>
+                <li>Email <a href="mailto:contact@sixteencolors.net">contact@sixteencolors.net</a></li>
+                <li>Feeds <a href="http://feeds.feedburner.com/SixteenColorsAnsiAndAsciiArchive-News">Sixteen Colors News</a><!-- <a href="#">Latest Artpacks</a> --></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
--- wp-content/themes/sixteencolors/header.php
+++ wp-content/themes/sixteencolors/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang=en>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <title>
@@ -22,5 +22,48 @@
 
 ?>
     </title>
+    <meta name="description" content="Sixteen Colors is an archive of ANSI art and ASCII art produced by groups in the underground digital artscene from 1990 to the present. This art began as a way to make Bulletin Board Systems more attractive but grew to be an art medium in its own right." />
+    <meta name="keywords" content="ANSI, ASCII, art, underground, textmode, bbs" />
+<!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 </head>
 <body>
+    <header class=container>
+        <h1 class=grid_4><a href="http://sixteencolors.net/">Sixteen Colors ANSI/ASCII Art Archive</a></h1>
+        <nav>
+            <ul id=nav>
+                <li class="grid_2"><a href="http://sixteencolors.net/info">Information</a>
+                    <ul>
+                        <li><a href="http://blog.sixteencolors.net">News</a></li>
+                        <li><a href="http://sixteencolors.net/history">History</a></li>
+                        <li><a href="http://sixteencolors.net/glossary">Glossary</a></li>
+                    </ul>
+                </li>
+                <li class=grid_2><a href="http://sixteencolors.net/art">Artwork</a>
+                    <ul>
+                        <li><a href="http://sixteencolors.net/pack">Packs</a></li>
+                        <li><a href="http://sixteencolors.net/group">Groups</a></li>
+                        <li><a href="http://sixteencolors.net/artist">Artists</a></li>
+                        <li><a href="http://sixteencolors.net/convert">Converter</a></li>
+                    </ul>
+                </li>
+                <li class=grid_2><a href="http://sixteencolors.net/site">Site</a>
+                    <ul>
+                        <li><a href="http://sixteencolors.net/contact">Contact</a></li>
+                        <li><a href="http://sixteencolors.net/stats">Statistics</a></li>
+                        <li><a href="http://sixteencolors.net/api">API</a></li>
+                        <li><a href="http://sixteencolors.net/colophon">Colophon</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id=search class=grid_5>
+            <h2>Search</h2>
+            <form>
+                <input type=search action="http://sixteencolors.net/search" name=q placeholder="Search the archive..." required /> 
+                <input type=submit value=Search />
+            </form>
+        </div>
+        <div class=clear></div>
+    </header>
