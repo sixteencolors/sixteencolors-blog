67ad08583fbf7720987b6da9c8ffc6200a15924c
Brian Cassidy <bricas@cpan.org>
2011-06-10 11:05:20 -0300

    searchform template. 8-4 grid split.

--- wp-content/themes/sixteencolors/index.php
+++ wp-content/themes/sixteencolors/index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <section class=container>
-    <div class=grid_9 id=blog>
+    <div class=grid_8 id=blog>
 <?php get_template_part( 'loop', 'index' ); ?>
     </div>
-    <div class=grid_3 id=sidebar>
+    <div class=grid_4 id=sidebar>
 <?php get_sidebar(); ?>
     </div>
     <div class=clear></div>
