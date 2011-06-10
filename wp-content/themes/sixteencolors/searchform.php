f0b74690ddae61ddf1883815c55427ecaf7f143a
Brian Cassidy <bricas@cpan.org>
2011-06-10 11:08:36 -0300

    searchform work

--- wp-content/themes/sixteencolors/searchform.php
+++ wp-content/themes/sixteencolors/searchform.php
@@ -1,8 +1,8 @@
-<form role="search" method="get" action="<?php home_url( '/' ) ?>" >
+<form role="search" method="get" action="<?php home_url( '/' ); ?>" >
 	<div>
     <label class="screen-reader-text" for="s">Search 16c News</label>
-	<input type=search value="<?php get_search_query() ?>" name="s" id="s" placeholder="Search 16c News..." required />
-	<input type=submit id="searchsubmit" value="<?php esc_attr('Search') ?>" />
+	<input type=search value="<?php get_search_query(); ?>" name="s" id="s" placeholder="Search 16c News..." required />
+	<input type=submit id="searchsubmit" value="Search" />
 	</div>
 </form>
 
