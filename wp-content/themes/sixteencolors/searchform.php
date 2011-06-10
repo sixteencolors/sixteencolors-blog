5e562f0e3e15a4511dbc469b021da5eef7175b69
Brian Cassidy <bricas@cpan.org>
2011-06-10 11:53:39 -0300

    searchbar

--- wp-content/themes/sixteencolors/searchform.php
+++ wp-content/themes/sixteencolors/searchform.php
@@ -1,6 +1,5 @@
-<form role="search" method="get" action="<?php echo home_url( '/' ); ?>" >
+<form method="get" action="<?php echo home_url( '/' ); ?>">
     <label class="screen-reader-text" for="s">Search 16c News</label>
-	<input type=search value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search 16c News..." required />
-	<input type=submit id="searchsubmit" value="Search" />
+	<input type=search  name=s id=s value="<?php echo get_search_query(); ?>" placeholder="Search 16c News..." required />
+	<input type=submit value=Search />
 </form>
-
